3f56547f8d8755123aeb614fc19704b98537ec97
Peter Phan <peter_phan05@yahoo.com>
2021-04-19 01:31:21 -0700

    updated header logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
-        <div>
-            Header
-        </div>
-    )
+        <Container>
+            <Main>
+                <AccessTimeIcon />
+                <SearchContainer>
+                    <Search>
+                        <input type="text" placeholder="Search..." />
+                    </Search>
+                </SearchContainer>
+                <HelpOutlineIcon />
+            </Main>
+
+            <UserContainer>
+                <Name>Peter</Name>
+                <UserImage>
+                    <img src="https://i.imgur.com/6VBx3io.png" alt="logo"/>
+                </UserImage>
+            </UserContainer>
+        </Container>
+    );
 }
 
-export default Header
+export default Header;
+
+const Container = styled.div`
+    background: #350d36;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Main = styled.div`
+    display: flex;
+`;
+
+const SearchContainer = styled.div``;
+
+const Search = styled.div``;
+
+const UserContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Name = styled.div``;
+
+const UserImage = styled.div``;
